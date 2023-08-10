9505041e1383558411a5dc2060b83c32c8d94c61
Timothy Allen <twlln@xtra.co.nz>
2023-08-10 14:02:48 +1200

    Updates through to Lab 2 task 3

--- Lab_2/PCA2.py
+++ Lab_2/PCA2.py
@@ -12,16 +12,12 @@ from read_station_data import read_station_data_file
 
 def load_all_data():
     "Loads all data in the dir Station_data into arrays lat,lon, and date and temp into a dataframe and returns them"
-    lat = []        # Initializing vars
-    lon = []
     start_date  = None
     end_date  = None
     time_ntemp_df = pd.DataFrame()
 
-    for path in glob.glob("Station_data/*/*"):
+    for path in glob.glob("Station_data/*/*")[:40]:
         cache = read_station_data_file(path)
-        lat.append(cache[0])
-        lon.append(cache[1])
         dates = cache[2]
         values = cache[3]
 
@@ -30,19 +26,20 @@ def load_all_data():
         if end_date  is None or max(dates) > end_date :
             end_date  = max(dates)
 
-        cache_df = pd.DataFrame(data=values, index=dates, columns=[path])   # Create a temporary dataframe for each file and interpolate missing values
+        meta_data_tuples = [(cache[0], cache[1], path)]     # Inserting latitude and longitude data into dataframe
+        column_data = pd.MultiIndex.from_tuples(meta_data_tuples, names=["Latitude", "Longitude", "Station"])
+
+        cache_df = pd.DataFrame(data=values, index=dates, columns=column_data)   # Create a temporary dataframe for each file and interpolate missing values
         cache_date_range = pd.date_range(start=start_date, end=end_date, freq='MS')
         cache_df = cache_df.reindex(cache_date_range)
-        cache_df.interpolate(method='linear', axis=0, inplace=True)
 
         time_ntemp_df = pd.concat([time_ntemp_df, cache_df], axis=1) # Merge the temporary dataframe into the main dataframe
-    return lat,lon, time_ntemp_df
+
+    return time_ntemp_df
 
 def main():
     "Do the thingy"
-    lat,lon, tt_df= load_all_data() # Overdone cos I misread the question but it works
-    #print(f'Latitude = {lat}')
-    #print(f'Longitude = {lon}')
-    #print(f'Temperature dataframe = {tt_df}')
+    tt_df= load_all_data() # Overdone cos I misread the question but it works
+    #print(f'Temperature dataframe:\n{tt_df}')
 
 main()
--- Lab_2/PCA3.py
+++ Lab_2/PCA3.py
@@ -1,22 +1,45 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
 import pandas as pd
 
-from library2 import Principal_Component_Analysis
+from read_station_data import datetime_array_create
+from PCA_artificial_data import Principal_Component_Analysis
 from PCA2 import load_all_data
 
+#z = np.ma.masked_values([1.0, 1.e20, 3.0, 4.0], 1.e20)
+#val = [0, 0, 5, 6, 5]
+#val_i = np.where(val == -99.0, True , False)
+#print(f'joe mama{val_i}')
+
+def invalid_data_stripper(tt_df):
+    """takes a dataframe, slices to dates between Jan 1990 and Dec 2005 then drops columns with invalid data(-99.0)
+    Input: dataframe with datatime indexes and filename columns
+    Returns: shortened dataframe with bad data removed
+    """
+    temperature_time_stripped = tt_df.loc['1990-01-01':'2005-12-01']
+    for column in temperature_time_stripped.columns:
+        if any(temperature_time_stripped[column].isin([-99.0])):
+            temperature_time_stripped = temperature_time_stripped.drop(columns = column)
+    return temperature_time_stripped
+
 def main():
-    "Does somtehing"
-    lat,lon, tt_df= load_all_data()
-    [eigen_values,eigen_vectors,anomaly,covariance]=Principal_Component_Analysis(tt_df.values)
-    PC=np.matmul(anomaly,eigen_vectors)
-    for j in [-1,-2,-3]:     # Plot the last three values in the PCA analysis - note ordering
-        plt.figure()
-        plt.subplot(2,1,1)
-        print(np.shape(eigen_vectors[:,j]))
-        plt.pcolor(np.reshape(eigen_vectors[:,j],(13,13)))  # Reshapes the eigenvectors to be plotted
-        plt.subplot(2,1,2)
-        plt.plot(PC[:,j])
-        plt.savefig('PCA2_Test_%01d.png' %j)  # ensures that ordering is nice
+    "Does something"
+    tt_df= load_all_data()
+    temperature_time_good = invalid_data_stripper(tt_df)
+
+    val_i = np.any(np.where(temperature_time_good == -99.0, True , False))
+    print(f'Invalid entries: {val_i}')    #check for an invalid entries
+
+    latitude, longitude, station = zip(*temperature_time_good.columns)
+    latitude_array, longitude_array, station_array = np. asarray(latitude), np. asarray(longitude), np. asarray(station)
+    temperature_array = temperature_time_good.to_numpy()    # Converts all pandas info to numyp arrays
+
+    np.savez(
+        "good_station_data",
+        latitude_array=latitude_array,
+        longitude_array=longitude_array,
+        station_array=station_array,
+        temperature_array=temperature_array)    # Saves arrays with names
+    
+main()
