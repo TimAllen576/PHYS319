6fa6fd2cec3e342229521c85d854ba14252bec59
twlln/gyvtuu576 <twlln@xtra.co.nz>
2023-09-17 00:46:25 +1200

    Complete interactive window plot

--- Project/Tauc_plotter.py
+++ Project/Tauc_plotter.py
@@ -9,11 +9,16 @@ Created Sept 2023
 @author: Timothy Allen 66522411
 """
 
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 from matplotlib.widgets import Slider
+
+
+LOWTFILE = "20230906 REAL554 PHYS381 LowT.csv"
+HIGHTFILE = "20230912 REAL554 PHYS381 HighT.csv"
 
 HEADER_LEN = 2   # Different header types would require redoing a lot of code
 #NUM_MEASURES = 3201 # Comment for preferred way of giving number of values
@@ -26,10 +31,14 @@ r=1/2
 
 
 def get_data(filename):
-    """Reads data from a spectroscopy file and returns numpy arrays for
+    """
+    Reads data from a spectroscopy file and returns numpy arrays for
     varying temps and wavelengths.
-    Input:  csv filename(string)
-    Returns:   dataframe with multi-index (filename and measurement)
+
+    Input: filename (string)
+    CSV filename.
+    Returns: formatted_data (DataFrame)
+    Dataframe with columns of wavelengths, baselines and abs data.
     """
     # Hacky, user prompts or automatic depending on performance requirements
     if 'NUM_MEASURES' in globals():
@@ -41,27 +50,29 @@ def get_data(filename):
         filename, header=[0,HEADER_LEN-1], nrows=rows)
 
     # Assume all samples are over same wavelengths so first column is taken
-    wavelengths = raw_data.loc[
-        :, (slice(None), "Wavelength (nm)")].iloc[:,0]
-    wavelengthsdf = pd.DataFrame(
-        wavelengths.values, columns=["Wavelength (nm)"])
+    wavelengths = raw_data.loc[:, (slice(None),
+                                   "Wavelength (nm)")].iloc[:,0]
+    wavelengthsdf = pd.DataFrame(wavelengths.values,
+                                columns=["Wavelength (nm)"])
 
-    # Does not preserve relative order of %T and Abs measurements
-    # Could use a loop or be sorted if necessary
+    # Creates new DataFrame, does not preserve relative order of 
+    # %T and Abs measurements, could use a loop or be sorted if necessary
     measurements = raw_data.loc[:, (slice(None), ["%T","Abs"])]
-
-    # Makes new dataframe with nice indexes
     new_cols = index_aligner(raw_data)
     measurementsdf = pd.DataFrame(measurements.values, columns=new_cols)
     formatted_data = wavelengthsdf.join(measurementsdf)
     return formatted_data
 
 def index_aligner(raw_data):
-    """Specialised function for reindexing particular dataframes.
+    """
+    Specialised function for reindexing particular dataframes.
     Shifts the index so filenames align with unit then merges the
     levels of the MultiIndex.
-    Input: Cary6000i csv with header as MultiIndex (Dataframe)
-    Returns: New column labels (List)
+
+    Input: raw_data (Dataframe)
+    Cary6000i csv data with header as MultiIndex.
+    Returns: new_cols (List)
+    Formatted list of proper column names for data.
     """
     top_labels = raw_data.columns.get_level_values(0)
     # Shift right 1
@@ -74,49 +85,46 @@ def index_aligner(raw_data):
     return new_cols
 
 def data_cleaner(lowTdata, highTdata):
-    """Specialised function, removes known bad data points and
+    """
+    Specialised function which removes known bad data points and
     seperates baseline data.
-    Input: Specific low and high temp data (two dataframes)
-    Returns: Combined data with known bad points removed (Dataframe)
-    and combined baseline data (Dataframe) 
+
+    Input: lowTdata (dataframe), highTdata (dataframe)
+    Specific low and high temp data with wavelengths, baselines and abs data.
+    Returns: allTdata (Dataframe)
+    Combined abs data with data after detector malfunction removed.
+    allbaselines (Dataframe)
+    Combined baseline data.
     """
     lowTbaseline = lowTdata.iloc[:,1:3]
     highTbaseline = highTdata.iloc[:,1:3]
     allbaselines = pd.concat([lowTbaseline, highTbaseline], axis=1)
 
-    # Slices tests, baselines and data where the detector? broke from heat
+    # Slices tests, baselines and data where the detector broke from heat?
     sliced_highTdata = highTdata.iloc[:,3:-1]
 
     malfunction_mask = np.zeros(sliced_highTdata.shape, dtype=bool)
     malfunction_mask[:801, -10:] = True
     clean_highTdata=sliced_highTdata.mask(malfunction_mask,other=np.nan)
     # Janky little line to stop errors from duplicate columns labels
-    clean_highTdata.rename(columns={"T325 (Abs)":"T325Hot (Abs)"}, inplace=True)
+    clean_highTdata.rename(columns={"T325 (Abs)":"T325NoVac (Abs)"}, inplace=True)
     allTdata = pd.concat(
         [lowTdata["Wavelength (nm)"],lowTdata.iloc[:,3:],
         clean_highTdata], axis=1)
     return allTdata, allbaselines
 
-def tauc_plotter(scaled_data):
-    """Plots multiple tauc plots on one set of axes.
-    Input:  dataframe with multi-index (filename and measurement)
-    Returns: None, shows plot
+def scale_n_differentiate(datadf):                                  # refactor?
     """
-    #formatting, fix calc first
-    for sample in scaled_data.columns[1:2:]:            # temp slice
-        plt.plot("eV", sample, data=scaled_data)
-    plt.xlabel("Photon energy (eV)")
-    plt.ylabel(r"$(\alpha h v)^{1/r}$")
-    plt.grid()
-    plt.savefig("Tauc-like plot")
-    #plt.show()
-
-def scale_n_differentiate(datadf):                                        # refactor
-    """Calculates optical band gaps for spectroscopic data using a
+    Calculates optical band gaps for spectroscopic data using a
     Tauc-like method.
-    Input:  dataframe with index as wavelengths and columns as data
-    Returns: scaled dataframe with photon energy, ahv^2 data, smoothed
-    data and the derivative of the data 
+
+    Input : datadf (DataFrame)
+    Clean data to be used for analysis with first column as wavelengths
+    and other columns as sample data.
+    Returns : sorted_scaled_df (DataFrame)
+    Data with columns of wavelengths scaled to photon energy,
+    absorbance scaled to ahv^2, 
+    derivative after data smoothing.
     """
     # Set params for smoothing filter
     window, p_order = 30, 1
@@ -134,81 +142,107 @@ def scale_n_differentiate(datadf):                                        # refa
         scaled_df["ahv_data",sample]=((
             datadf[sample_name] * scaled_df["Energy", "eV"]) ** (1/r))[
             :glass_int_index]
-        # Smooth data using preferred filter
-        scaled_df["ahv_data_smooth",sample] = smoother(
-            scaled_df["ahv_data",sample],window, p_order)
-        # Differentiate curve using numpy gradient
-        scaled_df["ahv_derivative", sample] = np.gradient(
-            scaled_df["ahv_data_smooth",sample],
-            scaled_df["Energy","eV"])
+        # Smooth data and find its derivative using preferred filter
+        scaled_df["data_deriv",sample] = smooth_n_deriv(
+            scaled_df["ahv_data", sample],
+            scaled_df["Energy","eV"],
+            window, p_order)
     sorted_scaled_df = scaled_df.sort_index(axis=1, level=0)
-    #smoothing-var opts, derivative rel max
-    # multiple options -max dev, regr fit within window, band calcs
+    #smoothing-various opts,
     return sorted_scaled_df
 
-def smoother(rough_data,window, porder):
-    """Smooths data using a filter ? sued for testing several.
-    Input:  Pandas Series of rough data
-    Returns: Pandas Series of smooth data
+def smooth_n_deriv(rough_data, energy,window, porder):
+    """
+    Smooths data using a Savitzky-Golay filter.
+
+    Input: rough_data (Series)
+    Spec data scaled to ahv. 
+    Returns: derived_data (Series)
+    Deritive of smoothed data.
     """
     smooth_data = signal.savgol_filter(rough_data,window, porder)
-    #smooth_data = signal
-    return smooth_data
+    derived_data = np.gradient(smooth_data,energy)
+    return derived_data
 
-def tauc_demo(scaled_df):
-    """Interactively shows different smoothing options
-    Input:  dataframe
-    Returns: band gaps (list of ints)
-    """
-    window, porder = 30, 1
+def band_gap_calculator():
+    """Calculates the band gap using Tauc-like plots as..."""
+    #derivative rel max
+    # multiple options -max dev, regr fit within window, band calcs
 
-    for sample_name in data.columns[1:2]:                   # temp slice
-        scaled_df[sample] = smoother(data[sample],window, porder)
+class InteractivePlot:
+    """Interactively shows different smoothing options to show Daniel."""
 
-        # Differentiate curve using with numpy gradient
-        deriv = np.gradient(data[sample], data["eV"])
-
-        fig=plt.figure()
-        ax=fig.subplots()
+    def __init__(self, scaled_df):
+        # Initialize data and plot
+        self.window, self.p_order= 30, 1
+        self.demo_data = pd.DataFrame(scaled_df.iloc[:,1])
+        self.sample = self.demo_data.columns[0][1]
+        self.energy = scaled_df["Energy","eV"]
+        self.fig, self.ax = plt.subplots()
         plt.subplots_adjust(bottom=0.25)
-        p,=ax.plot(data["eV"], deriv, "r-",linewidth=0.5)
-        # Defining the Slider position
-        ax_slide = plt.axes([0.25, 0.1, 0.65, 0.03])
-        # Properties of the slider
-        win_size = Slider(
-            ax_slide, 'Window size', valmin=1, valmax=99, valinit=30, valstep=1)
+        self.ax_slide = plt.axes([0.25, 0.1, 0.65, 0.03])
+        self.win_size = Slider(
+            self.ax_slide, 'Window size',
+            valmin=1, valmax=99, valinit=30, valstep=1)
+        self.ax.set_xlabel("Photon energy (eV)")
+        self.ax.set_ylabel("Gradient")
+        self.ax.grid(True)
 
-    fig=plt.figure()
-    ax=fig.subplots()
-    ax.plot(scaled_df["Energy", "eV"],
-            scaled_df["ahv_derivative", sample],
-            "r-",linewidth=0.5)
+        # Initial calculations
+        self.demo_data["data_deriv_raw", self.sample] = np.gradient(
+            self.demo_data["ahv_data", self.sample],
+            self.energy)
+        self.raw =self.ax.plot(self.energy,
+                self.demo_data["data_deriv_raw",self.sample],
+                "b.",markersize=1, alpha = 0.5)
+        self.demo_data["data_deriv",self.sample] = smooth_n_deriv(
+            self.demo_data["ahv_data", self.sample],
+            self.energy,
+            self.window, self.p_order)
+        self.current,=self.ax.plot(self.energy,
+                self.demo_data["data_deriv", self.sample],
+                "r-",linewidth=0.5)
+        
+        self.win_size.on_changed(self.update_win)
+
+    def update_win(self,window):
+        """Updates the plot with data smoothed with new window size."""
+        self.window = window
+        self.demo_data["data_deriv",self.sample] = smooth_n_deriv(
+            self.demo_data["ahv_data", self.sample],
+            self.energy,
+            self.window, self.p_order)
+        self.current.set_ydata(self.demo_data["data_deriv",self.sample])
+        self.fig.canvas.draw()
+
+    def show(self):
+        plt.show()
+
+def tauc_plotter(scaled_data):
+    """Plots multiple tauc plots on one set of axes.
+    Input:  dataframe with multi-index (filename and measurement)
+    Returns: None
+    Shows plot.
+    """
+    #formatting, fix calc first
+    for sample in scaled_data.columns[1:2:]:            # temp slice
+        plt.plot("eV", sample, data=scaled_data)
     plt.xlabel("Photon energy (eV)")
-    plt.ylabel("Gradient")
-    plt.grid(True)
-    plt.savefig(f"Derivative plot(presmoothed, {window, p_order})")
-    
-    win_size.on_changed(update)
-    plt.show()
-
-    return None
-
-# Updating the plot
-def update(val, window, ahv):
-    current_v = int(window.val)
-    new_y = signal.savgol_filter(y, current_v, 3)
-    p.set_ydata(new_y)
-    fig.canvas.draw() #redraw the figure
+    plt.ylabel(r"$(\alpha h v)^{1/r}$")
+    plt.grid()
+    plt.savefig("Tauc-like plot")
+    #plt.show()
 
 def main():
-    lowTfilename = "20230906 REAL554 PHYS381 LowT.csv"
-    highTfilename = "20230912 REAL554 PHYS381 HighT.csv"
-    lowTdata = get_data(lowTfilename)
-    highTdata = get_data(highTfilename)
+    """Main function to take UV-Vis spec data and create Tauc-like plots 
+    determining bandgap temperature dependance."""
+    lowTdata = get_data(LOWTFILE)
+    highTdata = get_data(HIGHTFILE)
     usingdata, allbaselines = data_cleaner(lowTdata, highTdata)
     scaled_data = scale_n_differentiate(usingdata)
-    print(scaled_data)
-    #tauc_demo(scaled_data)
+
+    demo = InteractivePlot(scaled_data)
+    demo.show()
     #tauc_plotter(scaled_data)
 
 if __name__ == "__main__":
