587c856caacf5631ca9322ee8e7aa16da9914eaa
twlln/gyvtuu576 <twlln@xtra.co.nz>
2023-09-23 05:23:15 +1200

    Final commit before leaving weekend. Code almost done except for using a window of derivatives and performing regression instead of taking max

--- Project/Tauc_plotter.py
+++ Project/Tauc_plotter.py
@@ -13,7 +13,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
-from scipy.ndimage import gaussian_filter
 from matplotlib.widgets import Slider
 
 LOW_T_FILE = "20230906 REAL554 PHYS381 LowT.csv"
@@ -24,8 +23,8 @@ WAVELENGTH_STEP = 0.25
 GLASS_INTERFERENCE_ENERGY = 5.2
 SAMPLE_THICKNESS = 44 * 10 ** - 7  # Units of cm
 R = 1 / 2  # r-value denoting the direct allowed transition
-FILTER_WINDOW = 31
-FILTER_ORDER = 1
+FILTER_WINDOW = 99
+FILTER_ORDER = 7
 
 
 def get_data(filename):
@@ -156,71 +155,110 @@ def smooth_n_derivative(rough_data, energy, window, porder):
     return derived_data
 
 
-# noinspection PyUnusedLocal
-def gaussian_smooth(rough_data, energy, sigma, *args):
-    """
-    Helper function to smooth data using a Gaussian filter.
-
-    Input: rough_data (Series)
-    Spec data scaled to ahv.
-    Returns: derived_data (Series)
-    Derivative of smoothed data.
-    """
-    smooth_data = gaussian_filter(rough_data, sigma, order=0)
-    derived_data = np.gradient(smooth_data, energy)
-    return derived_data
-
-
-def linear_region_extrapolater(scaled_df, test=False):
+def linear_region_extrapolater(scaled_df, average_ahv2):
     """
     Calculates the band gap using Tauc-like plots from a line built
-    from the max derivative. order should be around the width of the
-    derivative 'bump'.
+    from the max derivative.
 
     Input : scaled_df (DataFrame)
     Dataframe with columns of energy, ahv data and data derivatives
     for each temperature.
+    average_ahv2 (float)
+    Average ahv^2 value where derivative is maximum for low temperatures.
     Returns : linear_region_df (DataFrame)
     Dataframe of gradient and y-intercept of lines extrapolated from the
     linear region of the data, energy value at x-intercept is
     the optical band gap.
     """
-    # Approximate width of derivative peak around linear region
-    extrema_width = 50
     linear_region_df = pd.DataFrame()
-    for temperature in scaled_df["data_derivative"].columns[3:5]:  # temp slice
+    for temperature in scaled_df["data_derivative"].columns:
         derivative_array = np.array(scaled_df["data_derivative", temperature])
-
-        # Correct index is second to last, final is at far right and 
-        # since derivatives are so low at low energies
-        # many false peaks appear
-        max_index = signal.argrelextrema(derivative_array,
-                                         np.greater,
-                                         order=extrema_width)[0][
-            -2]  # testing slice
-
-        if test:
-            plt.plot(scaled_df["Energy", "eV"], derivative_array)
-            idx = max_index
-            plt.vlines(scaled_df["Energy", "eV"][idx],
-                       0, 60)
-            plt.vlines(scaled_df["Energy", "eV"][idx + extrema_width],
-                       0, 60, linestyles='dashed')
-            plt.vlines(scaled_df["Energy", "eV"][idx - extrema_width],
-                       0, 60, linestyles='dashed')
-            plt.grid(True)
-            plt.show()
-
-        max_derivative = derivative_array[max_index]
-        max_derivative_data = scaled_df["ahv_data", temperature][max_index]
-        max_derivative_energy = scaled_df["Energy", "eV"][max_index]
-        linear_region_y_int = max_derivative_data - max_derivative * max_derivative_energy
+        # Find index of ahv_data value closest to average ahv^2
+        max_index = (np.abs(scaled_df["ahv_data", temperature] -
+                            average_ahv2)).idxmin()
+        max_slope_data = scaled_df["ahv_data", temperature][max_index]
+        max_slope = derivative_array[max_index]
+        max_slope_energy = scaled_df["Energy", "eV"][max_index]
+        linear_y_int = max_slope_data - max_slope * max_slope_energy
         # Important to match multi-index
-        linear_region_df[
-            "ahv_data", temperature] = max_derivative, linear_region_y_int
-
-    # add option for regression fit within window
+        linear_region_df["ahv_data", temperature] = max_slope, linear_y_int
     return linear_region_df
+
+
+def ahv2extractor(scaled_df):
+    """
+    Extracts the average ahv^2 value where the derivative is a maximum
+    for low temperatures where the band gap presents as a significant
+    'bump'. Order should be around the width of the derivative 'bump'.
+
+    Input: scaled_df (DataFrame)
+    Dataframe with columns of energy, ahv data and data derivatives.
+    Returns: average_ahv2 (float)
+    Average ahv^2 value for low temperatures.
+    """
+    # Approximate width of derivative peak around linear region
+    bump_width = 75
+    linearity_requirement = 3 * 10 ** 11
+    last_bump_height = linearity_requirement
+    temperature_index = 1
+    ahv2list = []
+    while (
+            last_bump_height >= linearity_requirement) and (
+            temperature_index < len(scaled_df["data_derivative"].columns)):
+        temperature = scaled_df["data_derivative"].columns[temperature_index]
+        derivative_array = np.array(scaled_df["data_derivative", temperature])
+        # Correct index may be specific to the data
+        max_index = signal.argrelextrema(derivative_array, np.greater,
+                                         order=bump_width)[0][-1]
+        # # Visually determining bump width
+        # plt.plot(scaled_df["Energy", "eV"], derivative_array)
+        # plt.vlines(scaled_df["Energy", "eV"][max_index],
+        #            0, max(derivative_array))
+        # plt.vlines(scaled_df["Energy", "eV"][max_index + bump_width],
+        #            0, max(derivative_array), linestyles='dashed')
+        # plt.vlines(scaled_df["Energy", "eV"][max_index - bump_width],
+        #            0, max(derivative_array), linestyles='dashed')
+        # plt.grid(True)
+        # plt.show()
+        last_bump_height = (
+                derivative_array[max_index] -
+                derivative_array[max_index + bump_width])
+        ahv2list.append(scaled_df["ahv_data", temperature][max_index])
+        temperature_index += 1
+    average_ahv2 = np.mean(ahv2list)
+    return average_ahv2
+
+
+def tauc_plotter(scaled_df, linear_region_df):
+    """Plots multiple tauc plots on one set of axes.
+    Input:  scaled_df (DataFrame)
+    Dataframe with columns of energy, ahv data and data derivatives
+    linear_region_df (DataFrame)
+    DataFrame with columns of max derivative and y-intercept for each temp
+    Returns: None
+    Shows plot.
+    """
+    fig, ax = plt.subplots()
+    for temperature in scaled_df["ahv_data"].columns:
+        temperature_label = temperature.rstrip("(ahv)")
+        ax.plot(scaled_df[("Energy", "eV")],
+                scaled_df["ahv_data", temperature],
+                label=temperature_label)
+        # max_derivative, linear_region_y_int = linear_region_df[
+        #     "ahv_data", temperature]
+        # band_gap = -linear_region_y_int / max_derivative
+        # lin_region = max_derivative * scaled_df[
+        #     ("Energy", "eV")] + linear_region_y_int
+        # ax.plot(scaled_df[("Energy", "eV")], lin_region, "--",
+        #          label=f"{temperature_label} band-gap: {band_gap:.3f}")
+    ax.set_xlim(2.5, 4)
+    ax.set_ylim(0, 1.5 * 10 ** 12)
+    ax.set_xlabel("Photon energy (eV)")
+    ax.set_ylabel(r"$(\alpha h v)^{1/r}$")
+    ax.grid(True)
+    ax.legend()
+    plt.savefig("Tauc-like plot")
+    plt.show()
 
 
 class InteractivePlot:
@@ -239,12 +277,13 @@ class InteractivePlot:
         # Window size for Sav-gol filter should be odd
         self.win_size = Slider(
             self.ax_slide, 'Window size',
-            valmin=3, valmax=99, valinit=FILTER_WINDOW, valstep=2)
+            valmin=3, valmax=151, valinit=FILTER_WINDOW, valstep=2)
         self.porder_size = Slider(
             self.ax_slide_p_ord, 'Poly order',
-            valmin=0, valmax=9, valinit=FILTER_ORDER, valstep=1)
+            valmin=0, valmax=10, valinit=FILTER_ORDER, valstep=1)
         self.ax.set_xlabel("Photon energy (eV)")
-        self.ax.set_ylabel("Gradient")
+        self.ax.set_ylabel(r"d($\alpha E)^2$ / dE")
+        self.ax.set_title(self.temperature)
         self.ax.grid(True)
 
         # Initial calculations
@@ -252,8 +291,8 @@ class InteractivePlot:
             self.demo_data["ahv_data", self.temperature],
             self.energy)
         self.raw = self.ax.plot(self.energy,
-                                self.demo_data["data_derivative_raw",
-                                               self.temperature],
+                                self.demo_data[
+                                    "data_derivative_raw", self.temperature],
                                 "b.", markersize=1, alpha=0.5)
         self.demo_data[
             "data_derivative", self.temperature] = smooth_n_derivative(
@@ -265,6 +304,7 @@ class InteractivePlot:
                                      "r-", linewidth=0.5)
         self.win_size.on_changed(self.update_win)
         self.porder_size.on_changed(self.update_porder)
+        # plt.savefig("Gaussian derivative plot (sigma12, radius7)")
         plt.show()
 
     def update_win(self, window):
@@ -292,39 +332,6 @@ class InteractivePlot:
         self.fig.canvas.draw()
 
 
-def tauc_plotter(scaled_df, linear_region_df):
-    """Plots multiple tauc plots on one set of axes.
-    Input:  scaled_df (DataFrame)
-    Dataframe with columns of energy, ahv data and data derivatives
-    linear_region_df (DataFrame)
-    DataFrame with columns of max derivative and y-intercept for each temp
-    Returns: None
-    Shows plot.
-    """
-    for temperature in scaled_df["ahv_data"].columns:
-        temperature_label = temperature.rstrip("(ahv)")
-        plt.plot(scaled_df[("Energy", "eV")],
-                 scaled_df["ahv_data", temperature],
-                 label=temperature_label)
-
-        max_derivative, linear_region_y_int = linear_region_df[
-            "ahv_data", temperature]
-        band_gap = -linear_region_y_int / max_derivative
-        lin_region = max_derivative * scaled_df[
-            ("Energy", "eV")] + linear_region_y_int
-        plt.plot(scaled_df[("Energy", "eV")], lin_region, "--",
-                 label=f"{temperature_label} band-gap: {band_gap:.3f}")
-
-    plt.xlabel("Photon energy (eV)")
-    plt.xlim(0, 5.5)
-    plt.ylabel(r"$(\alpha h v)^{1/r}$")
-    plt.ylim(0, 80)
-    plt.grid(True)
-    plt.legend()
-    # plt.savefig("Tauc-like plot")
-    plt.show()
-
-
 def main():
     """Main function to take UV-Vis spec data and create Tauc-like plots
     determining band-gap temperature dependence."""
@@ -333,15 +340,15 @@ def main():
     clean_data, all_baselines = data_cleaner(low_t_data, high_t_data)
     scaled_df = scale_n_differentiate(clean_data)
 
-    # TODO: HW, clean up examples and code,
-    #  implement regression and set points,
-    #  finalize smoothing and give good examples
+    # TODO: HW, implement regression window
 
-    InteractivePlot(scaled_df.iloc[:, [0, 10]])
-    plt.show()
+    # InteractivePlot(scaled_df.iloc[:, [0, 15]])
+    # plt.show()
 
-    # linear_region_df = linear_region_extrapolater(scaled_df)
-    # tauc_plotter(scaled_df.iloc[:, [0,4,5]], linear_region_df)
+    average_ahv2 = ahv2extractor(scaled_df)
+    linear_region_df = linear_region_extrapolater(scaled_df, average_ahv2)
+    tauc_plotter(scaled_df.iloc[:, [0, 1, 2, 10, 20, 30, 33]],
+                 linear_region_df)
 
 
 if __name__ == "__main__":
